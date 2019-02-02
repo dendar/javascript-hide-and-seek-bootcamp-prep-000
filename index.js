b0907f600147e242218cc749af58957c5787c7f0
Nadir Sarigul <nadir.nyc62@gmail.com>
2019-02-02 16:03:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,10 +11,7 @@ function increaseRankBy(n){
   }
 }
 function deepestChild(){
-  var grandNode = document.querySelector('#grand-node');
-  var deepestNode =grandNode.children[0];
-  for(var i=0; !deepestNode.children[i]; i){
-    deepestNode=deepestNode.children[0]
+  var deep = document.getElementById('grand-node').querySelectorAll('div')
+  return deep[deep.length-1]
   }
-  return deepestNode;
-}
+  
